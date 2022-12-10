15208e32d3a05f6c7de865c685a1067370870af1
nabilraimi <contact.raiminabil@gmail.com>
2022-12-09 18:46:04 -0800

    Print Alphbt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: 0
+ *     */
+int main(void)
+{
+		int i;
+
+			for (i = 'A'; i <= 'Z'; i++)
+					{
+								if (i != 'Q' && i != 'E')
+											{
+															putchar(tolower(i));
+																	}
+									}
+				putchar('\n');
+					return (0);
+}
