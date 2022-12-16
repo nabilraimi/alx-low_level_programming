4b40ab857c4c69b9b49d813ceef325901b693f9f
nabilraimi <contact.raiminabil@gmail.com>
2022-12-16 15:57:53 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ *  * print_alphabet - writes the alphabet
+ *   *
+ *    */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		_putchar(tolower(i));
+	}
+	_putchar('\n');
+}
