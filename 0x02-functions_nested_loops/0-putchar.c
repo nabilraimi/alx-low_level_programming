1b30f0733ece7960503e5a61db465844ae5a2947
nabilraimi <contact.raiminabil@gmail.com>
2022-12-16 15:53:08 -0800

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	char text_[] = "_putchar";
+	int pos = 0;
+
+	for (pos = 0; pos <= 7; pos++)
+	{
+		_putchar(text_[pos]);
+	}
+	_putchar('\n');
+	return (0);
+}
