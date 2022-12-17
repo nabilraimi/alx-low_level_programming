62da065f68bac30da50267a4579099b6dbb4786c
nabilraimi <contact.raiminabil@gmail.com>
2022-12-16 17:37:46 -0800

    Print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ *  * print_diagonal - print diagonal
+ *   * @n: The number as distance
+ *    *
+ *     */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)																{
+																						_putchar(' ');
+																					}
+																					_putchar('\\');
+																					_putchar('\n');
+																				}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
