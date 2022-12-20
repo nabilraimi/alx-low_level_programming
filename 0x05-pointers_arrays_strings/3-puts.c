1eef0aaae1d913c2d359a74438d41f18023afe74
nabilraimi <contact.raiminabil@gmail.com>
2022-12-20 00:27:51 -0800

    Prints a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *  * _puts - prints the given string
+ *   * @str: string passing pointer
+ *    *
+ *     */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
